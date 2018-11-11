c556bbc5ec5751ecbbda150ab461f7ba28322330
agrojas <rojasagustin90@gmail.com>
2018-11-10 22:27:32 -0300

    Add code from storage lesson

--- crypto-zombie/zombiefeeding.sol
+++ crypto-zombie/zombiefeeding.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.4.25;
 
-// put import statement here
+import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
+  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+      require(msg.sender == zombieToOwner[_zombieId]);
+      Zombie storage myZombie = zombies[_zombieId];
+  }
 }
